45747061944232e7b6b0904bc88be1cb82fc6d7e
sumon-ray <raysumon220020@gmail.com>
2025-02-27 23:28:20 +0600

    delete unecessary file

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,7 +3,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import authReducer from '../features/auth/authSlice';
-import counterReducer from '../features/counter/counterSlice';
 import userReducer from "../features/user/userSlice";
 import AllusersReducer from "../features/user/userSlice";
 const persistConfig = {
@@ -15,7 +14,6 @@ const persistedAuthReducer = persistReducer(persistConfig, authReducer)
 export const store = configureStore({
   reducer: {
     [baseApi.reducerPath] : baseApi.reducer,
-    counter: counterReducer,
     user: userReducer,
     auth: persistedAuthReducer,
     users: AllusersReducer,
